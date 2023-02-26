a21b62e6aab94422be86d42f32d5b193038e6c7f
Xamman27 <xamman27@gmail.com>
2023-02-26 19:28:14 +0600

    Have problem with googleads

--- test_google.py
+++ test_google.py
@@ -1,7 +1,8 @@
 from selene.support.shared import browser
 from selene import be, have
 
-
+browser.config.browser_name = 'firefox'
+browser.config.hold_browser_open = True
 browser.open('https://google.com')
 browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
 browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
